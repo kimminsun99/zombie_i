146144fbff194a44559672647f46ab3ae7dfe2b1
ny527228 <84300001+ny527228@users.noreply.github.com>
2021-06-10 20:41:31 +0900

    zombie 3

---
+++ zombie 3/Assets/Script/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool canPlayerMove = true; // 플레이어의 움직임 제어
+
+    public static bool isOpenInventory = false; // 인벤토리 활성화
+
+    public static bool isNight = false;
+    public static bool isWater = false;
+    private bool flag = false;
+
+    public static bool isPause = false; // 메뉴가 호출되면 true 
+
+    private WeaponManager theWM;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpenInventory || isPause)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            canPlayerMove = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            canPlayerMove = true;
+        }
+
+
+        if (isWater)
+        {
+            if (!flag)
+            {
+                StopAllCoroutines();
+                StartCoroutine(theWM.WeaponInCoroutine());
+                flag = true;
+            }
+        }
+
+        else
+        {
+            if (flag)
+            {
+                flag = false;
+                theWM.WeaponOut();
+
+            }
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        theWM = FindObjectOfType<WeaponManager>();
+    }
+
+}
